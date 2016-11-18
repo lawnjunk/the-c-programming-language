59a3f449631cb30d4d6d53ec83a5086e36f55d77
Duncan Marsh <dwolfm@gmail.com>
2016-11-18 02:10:06 -0800

    bits in prog

--- 2_chapter/bits.c
+++ 2_chapter/bits.c
@@ -6,6 +6,8 @@ int set_bit(int num, int offset);
 void print_byte(int b, char *msg);
 int flip_bit(int num, int offset);
 int clear_bit(int num, int offset);
+int get_bits(int num, int p, int c);
+int set_bits(int num, int p, int c, int value);
 
 int main(){
   int n;
@@ -32,6 +34,11 @@ int main(){
   puts("\nflip_bit(-1, 5)");
   n = flip_bit(-1, 5);
   print_byte(-1, "BEFORE"); 
+  print_byte(n, "AFTER ");
+
+  puts("\n get_bits(12, 4, 2)");
+  n = get_bits(0x3b, 3, 3);
+  print_byte(0x3b, "BEFORE"); 
   print_byte(n, "AFTER ");
 
   return 0;
@@ -73,4 +80,23 @@ int flip_bit(int num, int offset){
   int mask = 1 << offset;
   return num ^ mask;
 }
+
+/* fron in get c bits at position p */
+int get_bits(int num, int p, int c){
+  int shift = num >> (p - c + 1);
+  print_byte(shift, "SHIFT");
+  int mask = -1 << c;
+  return shift & ~mask;
+}
+
+int set_bits(int num, int p, int c, int value){
+  return -1;
+}
+
+/*0000 0000*/
+/*0000 0101*/
+
+/*7654 3210*/
+/*0000 1100*/
+
 
